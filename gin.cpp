cf2222342082029a26969a9748452bd5f2cfe77a
will7007 <38322012+will7007@users.noreply.github.com>
2018-04-18 10:04:07 -0400

    Create gin.cpp

---
+++ gin.cpp
@@ -0,0 +1,13 @@
+#include"card.h"
+//I think we don't need to make a class for the decks as they are just going to be vectors of cards, just like the player's hand
+//Since the cards are going to be moving all over the place, they should be pointers
+
+#include<iostream> //for output
+#include<algorithm> //for shuffle
+#include<vector> //for decks
+
+using namespace std; //idealy we would use std::
+
+int main() {
+  return 0;
+}
