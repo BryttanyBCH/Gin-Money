dd6c44f498efaec5d98e27359a9fb9cec8c838d9
will7007 <38322012+will7007@users.noreply.github.com>
2018-04-18 10:15:39 -0400

    Update card.h

--- card.h
+++ card.h
@@ -3,10 +3,10 @@
 C D H S*/
 class Card {
     public:
-    Card(int cardNum,int cardType)
+    Card(int cardNum,int cardType) //give the constructor the number of the card type, and this constructor will save that number as a char (as described in the comment table at the top of this file) for comparison
     Card(int number,char type) //give the constructor the number of the card and the Letter of the card's type (3 of clubs is Cards(3,C)
     int getPointVal(); //getters
-    char getCardType();
+    char getCardType(); //the cardType is always saved as a character by the card class, even if the int cardType constructor is used
     int getCardNum();
     
     private:
