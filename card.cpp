6a859c932891b2d61325d351045c6688982f2a7d
EC2 Default User <ec2-user@ip-172-31-11-91.us-east-2.compute.internal>
2018-04-19 13:59:24 +0000

    The card class should be fully implimented now, with point values (that might change later), comparable values for the card face and card suit, and a nice displayable string with cardPrint().

--- card.cpp
+++ card.cpp
@@ -2,36 +2,41 @@
 1 2 3 4
 C D H S*/
 #include"card.h"
-
-Card::Card(int number,char type) {
-    cardNum=number;
-    cardType=type;
-    if(number > 10) {pointVal=10;}
-    else {pointVal=number;}
-}
-
+#include<iostream>
 Card::Card(int number,int type) {
     cardNum=number;
-    if(number > 10) {pointVal=10;}
-    else {pointVal=number;}
-    switch(cardType) { 
+    if(number > 10) {cardPoints=10;}
+    else {cardPoints=number;}
+    switch(type) { 
         case 1:
         cardType='C';
+        cardDisplay="♣";
         break;
         case 2:
         cardType='D';
+        cardDisplay="♦";
         break;
         case 3:
         cardType='H';
+        cardDisplay="♥";
         break;
         default:
         cardType='S';
+        cardDisplay="♠";
     }
+    if(!(number <= 10 && number > 1)) {
+        if(number == 13) {cardDisplay = "K" + cardDisplay;}
+        else if(number == 12) {cardDisplay = "Q" + cardDisplay;}
+        else if(number == 11) {cardDisplay = "J" + cardDisplay;}
+        else {cardDisplay = "A" + cardDisplay;}
+    }
+    else {cardDisplay = std::to_string(number) + cardDisplay;}
     //this switch turns the entered number value into a character 
     //that can be easily printed and compaired to other cards 
     //(when checking combos for scoring)
 }
 
-int Card::getPointVal() {return pointVal;}
+int Card::getCardPoints() {return cardPoints;}
 char Card::getCardType() {return cardType;}
 int Card::getCardNum() {return cardNum;}
+void Card::cardPrint() {std::cout << "[" << cardDisplay << "]";}
--- card.h
+++ card.h
@@ -1,16 +1,18 @@
 /*♣ ♦ ♥ ♠
 1 2 3 4
 C D H S*/
+#include<string>
 class Card {
     public:
     Card(int cardNum,int cardType); //give the constructor the number of the card type, and this constructor will save that number as a char (as described in the comment table at the top of this file) for comparison
-    Card(int number,char type); //give the constructor the number of the card and the Letter of the card's type (3 of clubs is Cards(3,C)
-    int getPointVal(); //getters
-    char getCardType(); //the cardType is always saved as a character by the card class, even if the int cardType constructor is used
     int getCardNum();
+    char getCardType(); //the cardType is always saved as a character by the card class, even if the int cardType constructor is used
+    int getCardPoints(); //getters
+    void cardPrint();
     
     private:
     int cardNum; //what is the card's number? (Ace=1, Jack=11, Queen=12, King=13)
-    char cardType; //what type of card is the card (clubs, diamonds, hearts, or spades)
-    int pointVal; //how many points is the card worth (if cardNum>10 then this is 10)
+    char cardType; //what type of suit card is the card (clubs, diamonds, hearts, or spades). Never printed, ony used to check player's input
+    int cardPoints; //how many points is the card worth (if cardNum>10 then this is 10)
+    std::string cardDisplay; //the card icon (♣ ♦ ♥ ♠). A J, K, or Q will be added (and number not printed, as it will be set to null)
 };
